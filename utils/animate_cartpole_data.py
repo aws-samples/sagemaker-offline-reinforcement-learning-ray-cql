5e25f4ce85012daf67bd389a93936fb927e5ef95
Walter Mayfield <waltmayf@amazon.com>
2023-05-15 17:11:56 +0000

    Updated commments

--- utils/animate_cartpole_data.py
+++ utils/animate_cartpole_data.py
@@ -1,3 +1,5 @@
+# This file creates a .gif animation based on a simulation run
+
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -13,8 +15,6 @@ import tqdm
 
 simulation_file = open('./offline-rl-end-to-end/data/simulation.json')
 simulation_dict = json.load(simulation_file)
-
-# print(json.dumps(simulation_dict, indent = 2))
 
 # x, x_dot, theta, theta_dot = self.state
 
@@ -34,7 +34,6 @@ arm_height = 0.2
 
 cartpole_base = Rectangle(xy_cart, base_width, base_height, color='cornflowerblue')  # Rectangle Patch
 cartpole_arm = Rectangle(tuple(map(operator.add, xy_cart, (base_width/2-arm_width/2, base_height/2))), arm_width, arm_height)  # Rectangle Patch
-# goal_location = Circle(xy_cart, base_height/4, color='green')  # Rectangle Patch
 goal_location = Rectangle(xy_cart, base_width, base_height/4, color='green')  # Rectangle Patch
 
 
@@ -52,9 +51,6 @@ gif_writer = animation.PillowWriter(fps=30)
 with gif_writer.saving(fig, f, dpi = 60):
   for i, step in tqdm.tqdm(enumerate(simulation_dict['steps']), total = len(simulation_dict['steps'])):
     ax.set_title(f'Cartpole Simulation. t={i}, Average Reward: {average_reward:.2f}\n{action_source}')
-    # logger.info(step)
-    # print(step)
-    # x, x_dot, theta, theta_dot, x_goal = step['obs']
     # x, x_dot, theta, theta_dot, x_goal = step[['cart_position','cart_velocity','pole_angle','pole_angular_velocity','goal_position']]
     xy_cart = (step['cart_position'], 0)
     
@@ -62,7 +58,6 @@ with gif_writer.saving(fig, f, dpi = 60):
     cartpole_base.set(xy=xy_cart)
     cartpole_arm.set(xy=tuple(map(operator.add, xy_cart, (base_width/2-arm_width/2, base_height/2))))
     cartpole_arm.set(angle=-step['pole_angle']*180/np.pi)
-    # goal_location.set(center = (step['goal_position'],0))
     goal_location.set(xy=(step['goal_position'],xy_cart[1]))
     
     # Update Drawing:
@@ -70,9 +65,4 @@ with gif_writer.saving(fig, f, dpi = 60):
     # Save Frame:
     gif_writer.grab_frame()
 
-
-
-# # saving to m4 using ffmpeg writer
-# writervideo = animation.FFMpegWriter(fps=60)
-# anim.save('increasingStraightLine.mp4', writer=writervideo)
 plt.close()
